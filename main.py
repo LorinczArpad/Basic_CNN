93529d8020cabe387433a9848b8cd8078d57df23
Lőrincz Árpád <lorincz.arpad@bergholz.hu>
2024-11-18 12:05:18 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,163 @@
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+import itertools
+import random
+
+
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import cv2
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras import layers,models
+from keras_preprocessing.image import ImageDataGenerator
+from keras.layers import Dense, Dropout
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.callbacks import Callback, EarlyStopping,ModelCheckpoint
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras import Model
+from tensorflow.keras.layers.experimental import preprocessing
+from tensorflow.keras.models import Sequential
+from tensorflow import keras
+from tensorflow.keras import layers
+
+from pathlib import Path
+import os.path
+
+
+from sklearn.metrics import classification_report, confusion_matrix
+sns.set_style('darkgrid')
+from helper_functions import create_tensorboard_callback, plot_loss_curves, unzip_data, compare_historys, walk_through_dir, pred_and_plot
+
+def main():
+    batch_size = 32
+    img_height = 180
+    img_width = 180
+
+    data_dir = "./input/sea-animals-image-dataste"
+    test_image_path = "./test_images/sea_ray.jpg"
+
+    train_ds = tf.keras.utils.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.2,
+    subset="training",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
+     
+    val_ds = tf.keras.utils.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.2,
+    subset="validation",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
+
+    class_names = train_ds.class_names
+
+    AUTOTUNE = tf.data.AUTOTUNE
+
+    train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+    val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+    normalization_layer = layers.Rescaling(1./255)
+
+    normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+    image_batch, labels_batch = next(iter(normalized_ds))  
+   
+    num_classes = len(class_names)
+
+    model = Sequential([
+    layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+    layers.Conv2D(16, 3, padding='same', activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.01)),
+    layers.BatchNormalization(),
+    layers.MaxPooling2D(),
+
+    layers.Conv2D(32, 3, padding='same', activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.01)),
+    layers.BatchNormalization(),
+    layers.MaxPooling2D(),
+
+    layers.Conv2D(64, 3, padding='same', activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.01)),
+    layers.BatchNormalization(),
+    layers.MaxPooling2D(),
+
+    layers.Dropout(0.2),
+    layers.GlobalAveragePooling2D(),
+    layers.Dense(128, activation='relu'),
+    layers.Dropout(0.4),
+    layers.Dense(num_classes, name="outputs")
+    ])
+
+    model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+    
+    early_stopping = EarlyStopping(
+    monitor='val_loss',  
+    patience=5,          
+    restore_best_weights=True  
+    )
+
+    epochs=35
+    history = model.fit(
+    train_ds,
+    validation_data=val_ds,
+    epochs=epochs,
+    batch_size=batch_size,
+    callbacks=[early_stopping],
+    )
+
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs_completed = len(history.history['accuracy'])
+    epochs_range = range(epochs_completed)
+
+
+    plt.figure(figsize=(8, 8))
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs_range, acc, label='Training Accuracy')
+    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+
+
+
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs_range, loss, label='Training Loss')
+    plt.plot(epochs_range, val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    plt.show()
+
+
+    img = tf.keras.utils.load_img(
+    test_image_path, target_size=(img_height, img_width)
+    )
+    img_array = tf.keras.utils.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0) 
+
+    predictions = model.predict(img_array)
+    score = tf.nn.softmax(predictions[0])
+
+    print(
+        "This image most likely belongs to {} with a {:.2f} percent confidence."
+        .format(class_names[np.argmax(score)], 100 * np.max(score))
+    )
+
+
+if __name__ == "__main__":
+    main()
+
