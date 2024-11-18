b2d8c0c40f04a35d8ef345b39cb69056432a4126
lorincz.arpad@stud.uni-obuda.hu <lorincz.arpad@stud.uni-obuda.hu>
2024-11-18 17:52:51 +0100

    [M] No need for that shit

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ from tensorflow.keras.callbacks import Callback, EarlyStopping,ModelCheckpoint
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras import Model
-from tensorflow.keras.layers.experimental import preprocessing
+
 from tensorflow.keras.models import Sequential
 from tensorflow import keras
 from tensorflow.keras import layers
@@ -69,15 +69,17 @@ def main():
     train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
     val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
-    normalization_layer = layers.Rescaling(1./255)
 
-    normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
    
    
     num_classes = len(class_names)
 
     model = Sequential([
-   
+    layers.Rescaling(1./255),
+    layers.Conv2D(8, 3, padding='same', activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.01)),
+    layers.BatchNormalization(),
+    layers.MaxPooling2D(),
+
     layers.Conv2D(16, 3, padding='same', activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.01)),
     layers.BatchNormalization(),
     layers.MaxPooling2D(),
@@ -86,13 +88,9 @@ def main():
     layers.BatchNormalization(),
     layers.MaxPooling2D(),
 
-    layers.Conv2D(64, 3, padding='same', activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.01)),
-    layers.BatchNormalization(),
-    layers.MaxPooling2D(),
-
-    layers.Dropout(0.2),
+    layers.Dropout(0.4),
     layers.GlobalAveragePooling2D(),
-    layers.Dense(128, activation='relu'),
+    layers.Dense(64, activation='relu'),
     layers.Dropout(0.4),
     layers.Dense(num_classes, name="outputs")
     ])
